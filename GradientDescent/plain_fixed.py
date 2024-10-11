132fcb83e9afa52cf09718b892a7fd4824fba9ae
Frida Lien <fridalie@uio.no>
2024-10-11 14:37:48 +0200

    switch from seed to rng. test plain_fixed with sklearn. Test not working yet

--- GradientDescent/plain_fixed.py
+++ GradientDescent/plain_fixed.py
@@ -4,12 +4,14 @@ from .gradient_descent import GD
 
 
 class PlainFixed(GD):
-    def __init__(self, eta: float, beta_len: int, max_iter: int = 50, tol: float = 10e-8) -> None:
+    def __init__(self, eta: float, beta_len: int, max_iter: int = 50, tol: float = 10e-8, rng: np.random.default_rng = None) -> None:
         super().__init__()
         self.eta = eta      # something about eigenvalues here???
         self.max_iter = max_iter
         self.beta_len = beta_len
         self.tol = tol
+        self.rng = rng
+        self.init_beta = None
 
     def perform(self, ) -> np.ndarray:
         """
@@ -24,10 +26,12 @@ class PlainFixed(GD):
         cost = 10
         # if self.gradient is None:
         #     self.calculate_gradient()
-        beta = np.random.randn(self.beta_len)
+        beta = self.rng.random(self.beta_len)
+        self.init_beta = beta
         i = 0
         while (cost > self.tol) and (i < self.max_iter):
             beta -= self.eta * self.gradient(beta)
             i += 1
+        print(i)
         return beta
 
