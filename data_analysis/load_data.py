079dd068214baacadd273e7602585f6223ab3cb0
irisbore <irisbore@gmail.com>
2024-10-30 09:14:48 +0100

    made one comment

--- data_analysis/load_data.py
+++ data_analysis/load_data.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 pd.set_option('display.max_columns', 35)
 
-data, target = load_breast_cancer(return_X_y = True, as_frame=True)
+data, target = load_breast_cancer(return_X_y = True, as_frame=True) # set as_frame = True if you dont want columns as series
 
 # See what the features are
 print(data.columns)
@@ -14,6 +14,6 @@ print((target.value_counts(normalize=True)))
 # Distribution  of features
 print(data.describe())
 
-# Look at the dtypes. Comment: No NaNs. All values are floats, so no encoding needed. 
-print(data.info())
+# Look at the dtypes. 
+print(data.info()) #Comment: No NaNs. All values are floats, so no encoding needed. 
 
