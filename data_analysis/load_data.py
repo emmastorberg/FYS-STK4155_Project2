33bb7bbb9a1051d651607515861369348e2f9b4c
irisbore <irisbore@gmail.com>
2024-10-30 09:10:26 +0100

    cancer data looks fine, no preprocessing needed. except maybe scaling

--- data_analysis/load_data.py
+++ data_analysis/load_data.py
@@ -1,6 +1,19 @@
 from sklearn.datasets import load_breast_cancer
 import pandas as pd
-data = load_breast_cancer(as_frame=True)
 
-# Get an impression of our data set
-print((data.target.value_counts(normalize=True)))
+pd.set_option('display.max_columns', 35)
+
+data, target = load_breast_cancer(return_X_y = True, as_frame=True)
+
+# See what the features are
+print(data.columns)
+
+# Get an impression of the target distribution in our data set to check that it is balanced
+print((target.value_counts(normalize=True)))
+
+# Distribution  of features
+print(data.describe())
+
+# Look at the dtypes. Comment: No NaNs. All values are floats, so no encoding needed. 
+print(data.info())
+
