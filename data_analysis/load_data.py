09f128882d0c268827461af060f2ae59bc8cea34
irisbore <irisbore@gmail.com>
2024-10-30 11:31:11 +0100

    quick and dirty heatmap of relationship with target

--- data_analysis/load_data.py
+++ data_analysis/load_data.py
@@ -1,11 +1,15 @@
 from sklearn.datasets import load_breast_cancer
+import matplotlib.pyplot as plt
+#plt.style.use('ggplot')
+import seaborn as sns
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
 pd.set_option('display.max_columns', 35)
 
-data, target = load_breast_cancer(return_X_y = True, as_frame=True) # set as_frame = True if you dont want columns as series
+data, target = load_breast_cancer(return_X_y = True, as_frame=True) # set as_frame = True if you don´t want columns as series
 
-# See what the features are
+# See what the features are 
 print(data.columns)
 
 # Get an impression of the target distribution in our data set to check that it is balanced
@@ -17,3 +21,14 @@ print(data.describe())
 # Look at the dtypes. 
 print(data.info()) #Comment: No NaNs. All values are floats, so no encoding needed. 
 
+# Correlation
+#print(data.corr(numeric_only=True))
+
+# Calculate the correlation between each feature and the target
+correlation_with_target = data.apply(lambda x: x.corr(target))
+
+# Plotting the heatmap
+plt.figure(figsize=(10, 10))
+sns.heatmap(correlation_with_target.to_frame(), annot=True, cmap='coolwarm', cbar=True)
+plt.title('Correlation of Features with Target')
+plt.show()
