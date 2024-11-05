6fd8fcd1de4004f7c55bbbf4c7d50aa2f1d947e6
irisbore <irisbore@gmail.com>
2024-11-05 19:54:03 +0100

    added docstring pytorch

--- torch_plot.py
+++ torch_plot.py
@@ -10,9 +10,32 @@ from sklearn.metrics import confusion_matrix
 import pandas as pd
 
 import utils
-
+torch.manual_seed(2002) 
+utils.aesthetic_2D()
 
 class NN(nn.Module):
+    """
+    A feedforward neural network model for binary classification.
+
+    Attributes:
+        output (nn.Linear): Output layer, producing a single value (binary classification).
+        layers (nn.ModuleList): List of hidden layers, each a fully connected layer.
+        activation_func (nn.Module): Activation function applied to the hidden layers.
+    
+    Args:
+        input_size (int): The number of input features.
+        node_size (int): The number of neurons in each hidden layer.
+        num_hidden_layers (int): The number of hidden layers in the network.
+        activation (str): The activation function to use in hidden layers. 
+                          Options are 'relu', 'leaky_relu', 'sigmoid'. 
+    
+    Raises:
+        ValueError: If the provided activation function is not one of the recognized types ('relu', 'leaky_relu', 'sigmoid').
+
+    Methods:
+        forward(x):
+            Passes input through the network, returning the predicted output.
+    """
     def __init__(self, input_size, node_size, num_hidden_layers, activation):
         super(NN, self).__init__()
 
@@ -50,6 +73,24 @@ class LogReg(nn.Module):
     
 
 def gridsearch_pytorch(X_train_tensor, X_test_tensor, y_train_tensor, y_test_tensor, input_size):
+    """
+    Performs a grid search to tune the hyperparameters of a neural network
+
+    Args:
+        X_train_tensor (Tensor): The training feature data (input) in tensor format.
+        X_test_tensor (Tensor): The test feature data (input) in tensor format.
+        y_train_tensor (Tensor): The training labels (output) in tensor format.
+        y_test_tensor (Tensor): The test labels (output) in tensor format.
+        input_size (int): The number of features in the input data.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing the accuracy results for each combination of the number of
+                      hidden layers and the number of nodes per hidden layer, pivoted for heatmap plotting.
+    
+    Note:
+        - The grid search only considers 1 activation function, 'leaky_relu', because it has previously performed best
+        - The model is trained for 50 epochs with stochastic gradient descent (SGD) and a learning rate of 0.1.
+    """
     cost_func = nn.BCELoss()
     lr = 0.1
     accuracy_dict = {
@@ -78,8 +119,8 @@ def gridsearch_pytorch(X_train_tensor, X_test_tensor, y_train_tensor, y_test_ten
                       accuracy = (predicted.eq(y_test_tensor).sum().item() / y_test_tensor.size(0)) * 100
 
                 # Append results to the dictionary
-                accuracy_dict['Number of Hidden Layers'].append(num_hidden_layers)
-                accuracy_dict['Number of Nodes per Hidden Layer'].append(node_size)
+                accuracy_dict['Number of Hidden Layers'].append(n_hidden_layers)
+                accuracy_dict['Number of Nodes per Hidden Layer'].append(n_nodes)
                 accuracy_dict['accuracy'].append(accuracy)
 
     df_accuracy = pd.DataFrame(accuracy_dict)
@@ -90,7 +131,24 @@ def gridsearch_pytorch(X_train_tensor, X_test_tensor, y_train_tensor, y_test_ten
     return heatmap_data
 
 
-def test_activation_pytorch(X_train_tensor, X_test_tensor, y_train_tensor, y_test_tensor, input_size, node_size = 40, num_hidden_layers = 1):
+def test_activation_pytorch(X_train_tensor, X_test_tensor, y_train_tensor, y_test_tensor, input_size, node_size = 41, num_hidden_layers = 1):
+    """
+    Tests different activation functions 'sigmoid', 'relu', and 'leaky_relu'
+
+    The model is trained using stochastic gradient descent (SGD) with a fixed learning rate of 0.1 for 50 epochs.
+
+    Args:
+        X_train_tensor (Tensor): The input features for the training set.
+        X_test_tensor (Tensor): The input features for the test set.
+        y_train_tensor (Tensor): The labels for the training set.
+        y_test_tensor (Tensor): The labels for the test set.
+        input_size (int): The number of features in the input data.
+        node_size (int, optional): The number of nodes in each hidden layer. Defaults to 41.
+        num_hidden_layers (int, optional): The number of hidden layers in the network. Defaults to 1.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing accuracy results for each activation function across epochs (for plotting)
+    """
     criterion = nn.BCELoss()
     learning_rate = 0.1
     accuracy_dict = {
@@ -140,6 +198,33 @@ def plot_grid_heatmap(df, save=False, annot = True, title=r"Accuracy for Number 
 
         
 def pytorch():
+    """
+    Runs a pipeline for training a neural network on the breast cancer dataset.
+
+    The process includes:
+    - Loading and splitting the data.
+    - Scaling the features using standardization.
+    - Running grid search for hyperparameter optimization.
+    - Running tests for different activation functions.
+    - Training a final neural network model based on selected hyperparameters.
+    - Evaluating the model and returning the predictions and accuracy.
+
+    Args:
+        None: All necessary data is loaded and processed within the function.
+
+    Returns:
+        tuple: A tuple containing:
+            - y_test (ndarray): The true labels for the test set.
+            - predicted (ndarray): The predicted labels for the test set from the final trained model.
+            - grid_df (pd.DataFrame): The results from the grid search, including accuracy for different hyperparameter configurations.
+            - activation_df (pd.DataFrame): The results from testing different activation functions, including accuracy for each function across epochs.
+    
+    Note:
+        - The model is trained using a fixed set of hyperparameters based on the grid search results, specifically:
+          `node_size=41`, `num_hidden_layers=1`, and `activation_func="leaky_relu"`.
+        - The final model uses Binary Cross-Entropy (BCELoss) and Stochastic Gradient Descent (SGD) with a learning rate of 0.1.
+        - The grid search and activation function testing are run for 50 epochs for computational efficiency, while the final model is trained for 100 epochs.
+    """
     X, y = utils.get_cancer_data()
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
@@ -156,14 +241,17 @@ def pytorch():
     grid_df = gridsearch_pytorch(X_train_tensor, X_test_tensor, y_train_tensor, y_test_tensor, input_size)
     activation_df = test_activation_pytorch(X_train_tensor, X_test_tensor, y_train_tensor, y_test_tensor, input_size)
 
-    node_size = 5
-    num_hidden_layers = 5
-    activation_func = torch.sigmoid
-    model = LogReg(input_size)
+    # Final model based on grid search
+    node_size = 41
+    num_hidden_layers = 1
+    activation_func = "leaky_relu"
     model = NN(input_size, node_size, num_hidden_layers, activation_func)
     criterion = nn.BCELoss()  # Binary Cross-Entropy loss
-    learning_rate = 0.01
+    learning_rate = 0.1
     optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+
+    # For comparison with logreg
+    #model= LogReg(input_size)
 
     # Training loop
     num_epochs = 100
@@ -221,8 +309,8 @@ def plot_activation(activation_df: pd.DataFrame, save=False):
 
 if __name__ == "__main__":
     y_test, predicted, accuracy_df, activation_df = pytorch()
-    #plot_cm(y_test, predicted, save=False)
-    plot_grid_heatmap(accuracy_df, save=True)
-    plot_activation(activation_df, save=True)
+    plot_cm(y_test, predicted, save=False) 
+    plot_grid_heatmap(accuracy_df, save=False)
+    plot_activation(activation_df, save=False)
     
 
